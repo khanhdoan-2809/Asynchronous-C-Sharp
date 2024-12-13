a790806eea4d875f52848c1dfd2663dc2f8c8ad0
khanhdoan-2809 <khanhdoan21.work@gmail.com>
2024-12-13 22:01:42 +0700

    add interlocked

--- CancellationToken/Program.cs
+++ CancellationToken/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 
 class Program
 {
---
+++ Interlocked/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static object lockObject = new object();
+    static void Main(string[] args)
+    {
+        long IncrementValue = 0;
+        long SumValue = 0;
+
+        Parallel.For(0, 10000, number =>
+        {
+            //Before lock Parallel 
+            lock (lockObject)
+            {
+                IncrementValue++;
+                SumValue += IncrementValue;
+            }
+            //After lock Parallel 
+        });
+
+        Console.WriteLine($"Increment Value With lock: {IncrementValue}");
+        Console.WriteLine($"Sum Value With lock: {SumValue}");
+        Console.ReadKey();
+    }
+}
